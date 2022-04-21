9c4b55dde0b05923c33ff096addb6f63705f2f1b
Akshat Shah <akshatshah_21@yahoo.in>
2022-04-21 23:20:59 +0530

    Downgrade react to 17.0.2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+ReactDOM.render(
+  // <React.StrictMode>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
