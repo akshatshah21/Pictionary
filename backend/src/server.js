f2debd757e00b059a31b82143fe55e1ab66c6b3b
Akshat Shah <akshatshah_21@yahoo.in>
2022-04-24 15:19:40 +0530

    Add prettier to backend, src directory

---
+++ backend/src/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { EventEmitter } = require("events");
+const bodyParser = require("body-parser");
+const sockets = require("./socket");
+
+global.round = new EventEmitter();
+global.games = {};
+global.BONUS = 250;
+global.MAX_POINTS = 500;
+
+const app = express();
+
+// CORS
+// TODO Make this more secure
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  // const roomID = req.query.id;
+  // res.render('index', { roomID });
+  return res.json("hello");
+});
+
+const server = app.listen(process.env.PORT || 5000, process.env.IP, () => {
+  console.log(`Server listening on port ${server.address().port}`);
+});
+
+sockets.init(server);
---
+++ backend/src/socket.js
@@ -0,0 +1,39 @@
+const socketio = require("socket.io");
+const Room = require("./controllers/Rooms");
+const Canvas = require("./controllers/Canvas");
+const Disconnect = require("./controllers/Disconnect");
+const Game = require("./controllers/Game");
+
+module.exports.init = (server) => {
+  const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true, //access-control-allow-credentials:true
+      optionSuccessStatus: 200,
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log("connected user");
+    socket.on("newPrivateRoom", (player) =>
+      new Room(io, socket).createPrivateRoom(player)
+    );
+    socket.on("joinRoom", async (data) => {
+      await new Room(io, socket).joinRoom(data);
+    });
+    socket.on("settingsUpdate", (data) =>
+      new Room(io, socket).updateSettings(data)
+    );
+    socket.on("drawing", (data) =>
+      new Canvas(io, socket).broadcastDrawing(data)
+    );
+    socket.on("clearCanvas", () => new Canvas(io, socket).clearCanvas());
+    socket.on("startGame", async () => {
+      await new Game(io, socket).startGame();
+    });
+    socket.on("getPlayers", async () => {
+      await new Game(io, socket).getPlayers();
+    });
+    socket.on("message", (data) => new Game(io, socket).onMessage(data));
+    socket.on("disconnect", () => new Disconnect(io, socket).onDisconnect());
+  });
+};
